56e9dcdf2c28a6fdb547c115454520d71f9de814
Dennis Hahn <dennis.hahn@clscl-00367-mac.local>
2015-11-14 09:32:41 -0300

    space shuttle falls

---
+++ Game/Assets/NaveController.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NaveController : MonoBehaviour {
+
+	public GameObject nave;
+	private bool landed;
+	private Vector3 initialAcceleration, gravityVector, translation;
+	private float gravity;
+
+	void Start () {
+		landed = false;
+		gravity = 0.2f;
+		gravityVector = Vector3.down * gravity;
+		initialAcceleration = Vector3.zero;
+	}
+	
+	void Update () {
+	
+		if (nave.transform.position.y <= -12.0f) 
+		{
+			landed = true;
+		}
+		
+		if(!landed)
+		{
+			translation = initialAcceleration + gravityVector * Time.fixedTime;
+			nave.transform.Translate(translation);
+		}
+	}
+}

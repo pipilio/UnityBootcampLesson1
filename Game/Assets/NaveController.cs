b21a3073ca5ade1f6c28fe2427702381fd2b5cb3
Dennis Hahn <dennis.hahn@clscl-00367-mac.local>
2015-11-15 23:26:23 -0300

    fixed postioning over time

--- Game/Assets/NaveController.cs
+++ Game/Assets/NaveController.cs
@@ -1,16 +1,19 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 public class NaveController : MonoBehaviour {
 
 	public GameObject nave;
 	private bool landed;
-	private Vector3 initialAcceleration, gravityVector, velocity, translation, acceleration;
+	private Vector3 initialAcceleration, gravityVector, velocity, translation, acceleration, thrusterVector;
 	public float gravity;
+	public Text velocityText, accelText, positionText;
 
 	void Start () {
 		landed = false;
 		gravityVector = Vector3.down * gravity;
+		thrusterVector = Vector3.zero;
 		initialAcceleration = Vector3.zero;
 	}
 	
@@ -21,11 +24,22 @@ public class NaveController : MonoBehaviour {
 			landed = true;
 		}
 		
+		if ( Input.GetKey("space") )
+		{
+			thrusterVector = Vector3.up * 10.0f;
+			Debug.Log ("going up");
+		} else {
+			thrusterVector = Vector3.zero;
+		}
+		
 		if(!landed)
 		{
-			acceleration = gravityVector;
+			acceleration = gravityVector + thrusterVector;
 			velocity = velocity + acceleration * Time.deltaTime;
-			translation = translation + velocity * Time.deltaTime;
+			translation = velocity * Time.deltaTime;
+			velocityText.text = velocity.y.ToString();
+			accelText.text = Time.fixedTime.ToString();
+			positionText.text = translation.y.ToString();
 			nave.transform.Translate(translation);
 		}
 	}
